5fbe70d656a46e1882bf2e5963861cec3092673e
Gilberto L. Perez <gilberto.perez@uci.edu>
2017-02-04 15:59:27 -0800

    SortTest almost 100% done for grading. Added a short readme.

--- src/cs331/sorting/test/SortTest.java
+++ src/cs331/sorting/test/SortTest.java
@@ -1,8 +1,9 @@
 package cs331.sorting.test;
+
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
@@ -12,39 +13,72 @@ import cs331.sorting.Sort;
 import cs331.sorting.glp.*;
 
 public class SortTest {
+	
+	private static final int SIZE =  (int)Math.pow(2, 16);
 
 	@Test
-	public void sortTest() {
-		int n = 10;
-		List<Integer> unsorted = new LinkedList<>();
+	public void testInsertionSort()
+	{
+		List<Integer> unsorted = genUnsortedList(SIZE);
 		
-		Random gen = new Random(System.currentTimeMillis());
+		Sort<Integer> insertionSort = new InsertionSort<>();
 		
-		for(int i = 0; i < n; i++)
-			unsorted.add(gen.nextInt());
+		sortTest(unsorted, insertionSort);
+	}
+	
+	@Test
+	public void testMergeSort()
+	{
+		List<Integer> unsorted = genUnsortedList(SIZE);
 		
-		System.out.println(unsorted);
+		Sort<Integer> mergeSort= new MergeSort<>();
 		
-		Sort<Integer> s = new MergeSort<>();
+		sortTest(unsorted, mergeSort);
+	}
+	@Test
+	public void testMergeInsertionSort()
+	{
+		List<Integer> unsorted = genUnsortedList(SIZE);
+		
+		Sort<Integer> mergeInsertionSort= new MergeInsertionSort<>();
+		
+		sortTest(unsorted, mergeInsertionSort);
+	}
+	public <T extends S, S extends Comparable<T>> void sortTest(List<T> unsorted, Sort<T> s) {
 		s.init(unsorted);
 
-		List<Integer> sorted = s.getSortedList();
-		
+		List<T> sorted = s.getSortedList();
+
+		// Does the sorted list contain every element in the unsorted list?
+		assertTrue(sorted.containsAll(unsorted));
+		// Is the list actually sorted?
 		assertTrue(SortTest.isListSorted(sorted));
 	}
-	private static <T extends Comparable<T>> boolean isListSorted(List<T> list)
-	{
+
+	private static <T extends S, S extends Comparable<T>> boolean isListSorted(List<T> list) {
 		Iterator<T> i = list.iterator();
 		T prev = i.next();
-		
-		while(i.hasNext())
-		{
+
+		while (i.hasNext()) {
 			T next = i.next();
-			if(next.compareTo(prev) < 0)
+			if (next.compareTo(prev) < 0)
+			{
+				System.out.println("Found offending values: " + next + " and " + prev);
 				return false;
+			}
 			prev = next;
 		}
 		return true;
 	}
 
+	private static List<Integer> genUnsortedList(int n) {
+		List<Integer> unsorted = new ArrayList<>(n);
+
+		Random gen = new Random(System.currentTimeMillis());
+
+		for (int i = 0; i < n; i++)
+			unsorted.add(gen.nextInt());
+
+		return unsorted;
+	}
 }
